e5f85d25087b571b4fcdcd9faa90de6d103cf1ef
e-schultz <evan@rangle.io>
2020-01-23 18:31:38 -0500

    setup

--- config.js
+++ config.js
@@ -1,14 +1,14 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://evanjustevan.com',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
-  subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+  title: 'Evan Schultz',
+  subtitle: 'just evan',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'UA-156909480-1',
   useKatex: false,
   menu: [
     {
@@ -25,25 +25,25 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Evan Schultz',
     photo: '/photo.jpg',
-    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
+    bio: 'just evan.',
     contacts: {
-      email: '',
+      email: 'hello@evanjustevan.com',
       facebook: '#',
       telegram: '#',
-      twitter: '#',
-      github: '#',
+      twitter: 'e_p82',
+      github: 'e-schultz',
       rss: '',
       vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
+      linkedin: 'evanschultz1',
+      instagram: 'sir_eeps',
       line: '',
       gitlab: '',
       weibo: '',
       codepen: '',
       youtube: '',
-      soundcloud: '',
+      soundcloud: 'e-s82'
     }
   }
 };
